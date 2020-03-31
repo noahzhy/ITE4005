d6bb0361ec61a9d71fec4fcedc5ecd6b83ca5777
noahzhy <noahzhy@gmail.com>
2020-04-01 00:48:18 +0900

    fix

--- projects/project 1/apriori.py
+++ projects/project 1/apriori.py
@@ -25,44 +25,43 @@ def apriori(data=read_data(), support=args.min_sup/100, k=2):
         for tid in data:
             for can in candidates:
                 if can.issubset(tid):
-                    candidate_count[can] += 1 
-        res = {}
+                    candidate_count[can] += 1
+        res = dict()
         for key, value in candidate_count.items():
-            if float(value)/len(data) >= support:
-               res[key] = value/len(data)
+            if float(value/len(data)) >= support:
+               res[key] = float(value/len(data))
         return res
 
-    def item_set(can, k=2):
+    def item_set(item, k=2):
         new_candidates = []
-        itemset = scan_data(candidates=can)
         for i in itemset:
             for j in itemset:
                 if len(i.union(j)) == k:
                     new_candidates.append(i.union(j))
-        return set(new_candidates), itemset
+        candidates.update(set(new_candidates))
+        return itemset
 
-    res = {}
+    res = dict()
     while candidates:
-        candidates, res[k-1] = item_set(candidates, k)
+        print(candidates)
+        itemset = scan_data(candidates=candidates)
+        res[k-1] = item_set(itemset, k)
         k += 1
     return res
 
 
-def rules(freq, confidence=.0):
-    def f(item):
-        return freq[len(item)][item]
+def rules(res):
+    def get_support(item):
+        return res[len(item)][item]
 
-    for key, value in freq.items():
-        if key >= 1:
-            for item in value:
-                k = [combinations(item, i) for i, e in enumerate(item, 1)]
-                for element in map(frozenset, chain(*k)):
-                    remain = item.difference(element)
-                    if remain:
-                        (a, b), (c, d) = f(item).as_integer_ratio(), f(element).as_integer_ratio()
-                        conf = (a/b)/(c/d)
-                        if conf >= confidence:
-                            yield element, remain, round(f(item) * 100, 2), round(conf * 100, 2)
+    for key, value in res.items():
+        for item in value:
+            elements = [combinations(item, i) for i, e in enumerate(item, 1)]
+            for ele in map(frozenset, chain(*elements)):
+                diff = item.difference(ele)
+                if diff:
+                    conf = get_support(item) / get_support(ele)
+                    yield ele, diff, round(get_support(item)*100, 2), round(conf*100, 2)
 
 
 def write_data(rules, path=args.output_file):
@@ -75,7 +74,7 @@ def write_data(rules, path=args.output_file):
                 conf
             ))
 
+
 if __name__ == "__main__":
-    res = apriori()
-    rules = list(rules(res))
-    write_data(rules)
+    res = rules(apriori())
+    write_data(res)
