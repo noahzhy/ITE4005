4580fe01590de103c3c3489c95798728392aff68
noahzhy <noahzhy@gmail.com>
2020-05-25 16:05:38 +0900

    add

--- projects/project03/clustering.py
+++ projects/project03/clustering.py
@@ -4,20 +4,11 @@ from math import sqrt
 from collections import defaultdict
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input", type=str, default='input2.txt')
-parser.add_argument("n", type=int, default=5)
-parser.add_argument("eps", type=int, default=2)
-parser.add_argument("minPts", type=int, default=7)
-args = parser.parse_args()
-
-
 class Point:
     def __init__(self, args):
         self.id = int(args[0])
         self.x = args[1]
         self.y = args[2]
-        # -1: Noise; other num: cluster
         self.type = None
 
 
@@ -28,43 +19,47 @@ class DBSCAN:
         self.minPts = minPts
         self.clusters = defaultdict(list)
 
-    def dist(self, p1, p2):
-        return sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
+    def regionQuery(self, curr_p):
+        def dist(p1, p2):
+            return sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
 
-    def get_neighbor(self, curr_pt):
-        neighbor = [p for p in self.data if self.dist(curr_pt, p) <= self.eps]
-        neighbor.remove(curr_pt)
-        return neighbor
-
-    def expand(self, p, neighbors, count):
-        self.clusters[count].append(p)
-        for q in neighbors:
-            if not q.type:
-                q.type = count
-                neighbor = self.get_neighbor(q)
-                if len(neighbor) >= self.minPts:
-                    [neighbors.append(n) for n in neighbor]
-            if not (q in self.clusters[count]):
-                self.clusters[count].append(q)
+        return [p for p in self.data if dist(p, curr_p) <= self.eps]
 
     def clustering(self):
-        cluster_count = 0
+        count_cluster = 0
         for p in self.data:
             if p.type: continue
-            p.type = cluster_count
-            neighbors = self.get_neighbor(p)
+            p.type = count_cluster
+            neighbors = self.regionQuery(p)
             if len(neighbors) < self.minPts:
                 p.type = -1
             else:
-                cluster_count += 1
-                self.expand(p, neighbors, cluster_count)
+                count_cluster += 1
+                self.clusters[count_cluster].append(p)
+                for q in neighbors:
+                    if not q.type:
+                        q.type = count_cluster
+                        neighbor = self.regionQuery(q)
+                        if len(neighbor) >= self.minPts:
+                            [neighbors.append(n) for n in neighbor]
+                    if not (q in self.clusters[count_cluster]):
+                        self.clusters[count_cluster].append(q)
+
         return self.clusters
 
 
 if __name__ == "__main__":
-    data_list = np.loadtxt(args.input).tolist()
-    data = list(map(Point, data_list))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str)
+    parser.add_argument("n", type=int)
+    parser.add_argument("eps", type=int)
+    parser.add_argument("minPts", type=int)
+    args = parser.parse_args()
+
+    data = list(map(Point, np.loadtxt(args.input).tolist()))
     clusters = DBSCAN(data, args.eps, args.minPts).clustering()
+    dict_slice = lambda adict, start, end: { k:adict[k] for k in list(adict.keys())[start:end] }
+    clusters = dict_slice(clusters, 0, args.n)
 
     for key, value in clusters.items():
         np.savetxt(
